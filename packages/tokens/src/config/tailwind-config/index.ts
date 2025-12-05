9eaaa637aff8840a721dc72a8344a18e5460f21b
kyh0726 <younghyun75398@gmail.com>
2025-12-06 01:14:47 +0900

    chore: 객체명 causwContent로 명시적으로 변경

--- packages/tokens/src/config/tailwind-config/index.ts
+++ packages/tokens/src/config/tailwind-config/index.ts
@@ -1,3 +1,3 @@
 export { causwConfig } from './tailwind.config';
 export { causwPreset } from './tailwind-preset';
-export { tailwindContent } from './tailwind-content';
+export { causwContent } from './tailwind-content';
--- packages/tokens/src/config/tailwind-config/tailwind-content.ts
+++ packages/tokens/src/config/tailwind-config/tailwind-content.ts
@@ -1,4 +1,4 @@
-export const tailwindContent = [
+export const causwContent = [
   './node_modules/@causw/components/dist/**/*.{js,mjs}',
   './node_modules/@causw/tokens/dist/**/*.{js,mjs}',
 ];
