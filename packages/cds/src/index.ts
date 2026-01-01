0e7f4286afdbbdcf537414e20b9a7ac9eb21e734
kyh0726 <younghyun75398@gmail.com>
2026-01-01 22:34:45 +0900

    feat: 이전의 components 패키지 참조 제거

--- packages/cds/src/index.ts
+++ packages/cds/src/index.ts
@@ -3,14 +3,11 @@
  * 모든 디자인 시스템 모듈을 포함하는 메인 패키지
  */
 
-// Core utilities and logging
+// Core (components + utilities)
 export * from '@causw/core';
 
 // Design tokens
 export * from '@causw/tokens';
-
-// UI Components
-export * from '@causw/components';
 
 // Icons
 export * from '@causw/icons';
